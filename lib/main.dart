0a817c8437785d9e500ea1b1721fa07cbc615343
clshu <ccss0630@yahoo.com>
2021-06-12 22:00:54 -0700

    1. Create a StatefulWidget Ball. 2. Assign Ball() to body. 3. Add Image to Container. 5 Replace Container with Center to center it.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,20 @@ void main() => runApp(
       BallPage()
     );
 
+class Ball extends StatefulWidget {
+  // const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Image.asset('images/ball1.png'));
+  }
+}
+
 class BallPage extends StatelessWidget {
   // const BallPage({Key? key}) : super(key: key);
 
@@ -16,7 +30,7 @@ class BallPage extends StatelessWidget {
           title: Text('Ask Me Anything'),
           backgroundColor: Colors.blue[900],
         ),
-        body: Container(),
+        body: Ball(),
       )
     );
   }
