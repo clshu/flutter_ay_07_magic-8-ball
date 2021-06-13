b7ab1b976cbcdf0a33f07ae762c8331006382931
clshu <ccss0630@yahoo.com>
2021-06-12 21:47:37 -0700

    1. Create a StatelessWidget BallPage. 2. Add Scaffold and background color. 3. Add AppBar with a title and backgroundColor.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+      BallPage()
     );
+
+class BallPage extends StatelessWidget {
+  // const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Container(),
+      )
+    );
+  }
+}
